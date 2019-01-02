c090edfbdee78ce187307f85edf28b422405dc5b
racinel200 <37580871+racinel200@users.noreply.github.com>
2019-01-02 10:40:13 -0600

    Update DeviceManagerV6.py

--- DeviceManagerV6.py
+++ DeviceManagerV6.py
@@ -105,7 +105,7 @@ def login():
         user = User()
         user.id = email
         flask_login.login_user(user)
-        return flask.redirect(flask.url_for('DeviceManager/GetDeviceStatus'))
+        return flask.redirect("/DeviceManager/GetDeviceStatus")
     
     return 'Bad login'
 
