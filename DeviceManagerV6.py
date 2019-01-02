47609964e34b40c95345bbe3011a06727f24cc94
racinel200 <37580871+racinel200@users.noreply.github.com>
2019-01-02 13:42:50 -0600

    Update DeviceManagerV6.py

--- DeviceManagerV6.py
+++ DeviceManagerV6.py
@@ -96,7 +96,7 @@ def check_auth(username, password):
     """This function is called to check if a username /
     password combination is valid.
     """
-    return username == 'admin' and password == 'secret'
+    return username == APIUsername and password == APIPassword
 
 def authenticate():
     """Sends a 401 response that enables basic auth"""
